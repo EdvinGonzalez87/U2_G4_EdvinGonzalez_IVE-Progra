91b79ee4c3f4661823e4816e92685ec1bf9c9520
EdvinGonzalez87 <agonzalezdb23@gmail.com>
2026-04-27 12:12:19 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int TOTAL_SELLOS = 8;
+
+        Console.WriteLine("Bienvenido a la barberia PRESI");
+        for (int i = 1; i < TOTAL_SELLOS; i++) 
+        { 
+            Console.WriteLine("\nSellos actuales: {0} de {1}", i, TOTAL_SELLOS);
+            Console.WriteLine("Presiones cualquier tecla para registrar esta visita");
+            Console.ReadKey();  
+            Console.WriteLine(">>>Sellos, #{0}registrado correctamente",i);
+        }
+        Console.WriteLine("\n**************************************************");
+        Console.WriteLine("Cupon completado");
+        Console.WriteLine("Edvin, te has ganado un corte de caballero GARTIS...");
+        Console.WriteLine("**************************************************");
+    }
+}
