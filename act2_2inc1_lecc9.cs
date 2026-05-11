de8fae6d9ce77cdf8ff92937e1bf61de84b504cd
EdvinGonzalez87 <agonzalezdb23@gmail.com>
2026-05-10 19:39:24 -0600

    Add files via upload

---
+++ act2_2inc1_lecc9.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int monedas;
+
+        Console.Write("Ingrese la cantidad de monedas del jugador: ");
+        monedas = int.Parse(Console.ReadLine());
+
+        if (monedas < 350)
+        {
+            Console.WriteLine("Nivel 5 bloqueado. Necesitas al menos 350 monedas.");
+        }
+        else
+        {
+            Console.WriteLine("¡Nivel 5 desbloqueado! Puedes ingresar al nuevo nivel.");
+        }
+    }
+}
---
+++ act2_2inc1_lecc9.psc
@@ -0,0 +1,12 @@
+Algoritmo sin_titulo
+	Definir monedas Como Entero
+	
+    Escribir "Ingrese la cantidad de monedas del jugador:"
+    Leer monedas
+	
+    Si monedas < 350 Entonces
+        Escribir "Nivel 5 bloqueado. Necesitas al menos 350 monedas."
+    Sino
+        Escribir "¡Nivel 5 desbloqueado! Puedes ingresar al nuevo nivel."
+    FinSi
+FinAlgoritmo
